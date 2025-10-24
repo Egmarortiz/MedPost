93a3c496b77272c7cf1c12a48ec25e0d9ee60765
Egmarortiz <egmarocasio9@gmail.com>
2025-10-24 17:30:19 -0400

    consolidation

--- medpost_demo/quick_check.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine, inspect
-engine = create_engine("postgresql+psycopg://medpost:ChangeMe123!@127.0.0.1:5432/medpost_demo", future=True)
-insp = inspect(engine)
-print(insp.get_table_names())
